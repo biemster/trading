996936ea8c8bb8c4a094ef74abd026e502428920
Lars Beemster <l.j.beemster@gmail.com>
2017-02-05 20:59:15 +0100

    gold stuff

--- gold.py
+++ gold.py
@@ -11,6 +11,12 @@ if __name__ == "__main__":
 
     spreads = pandas.DataFrame(GC[1]['USD (AM)'] - GC[2]['Open'])
     spreads.rename(columns={'Open':'SPOT:FUT1'}, inplace=True)
+    for x in xrange(1,11):
+        spreads['GC%d'%x] = pandas.Series(GC[1]['USD (AM)'] - GC[x+1]['Open'])
 
-    spreads["2016":].plot()
+    for x in xrange(1,11):
+        print 'month ' + str(x) + ' averages:'
+        for y in ['2001','2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2012','2013','2014','2015','2016']:
+            print y + ': ' + str(spreads['GC%d'%x][y:].mean())
+
     plt.show()
