a39b2cdc1b292cfabf2d55750d0d03c89b572a7f
Lars Beemster <l.j.beemster@gmail.com>
2017-02-13 23:54:27 +0100

    figured out creating time series from dataframe

--- gold_ticks.py
+++ gold_ticks.py
@@ -8,9 +8,13 @@ if __name__ == "__main__":
 
     all_ticks_bid = (all_ticks.type == 'BID')
     all_ticks_ask = (all_ticks.type == 'ASK')
+    no_empty_price = (all_ticks.price > 0)
     xauusd_symbol = (all_ticks.symbol == 'XAUUSD')
 
-    xauusd_bid = pandas.Series(all_ticks[all_ticks_bid][xauusd_symbol].price)
+    xauusd_bid = pandas.Series(all_ticks[all_ticks_bid][xauusd_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
+    xauusd_ask = pandas.Series(all_ticks[all_ticks_ask][xauusd_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
 
-    print all_ticks[all_ticks_bid][xauusd_symbol]
     print xauusd_bid
+    xauusd_bid.plot()
+    xauusd_ask.plot()
+    plt.show()
