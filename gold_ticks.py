b2b49d3e7a8a6d1aabe15cd004693c06c1e338b5
Lars Beemster <l.j.beemster@gmail.com>
2017-02-15 17:09:20 +0100

    add diff calc

--- gold_ticks.py
+++ gold_ticks.py
@@ -11,26 +11,29 @@ if __name__ == "__main__":
     no_empty_price = (all_ticks.price > 0)
     xauusd_symbol = (all_ticks.symbol == 'XAUUSD')
     gc1_symbol = (all_ticks.symbol == 'GCG7')
-    gc2_symbol = (all_ticks.symbol == 'GCH7')
+    gc2_symbol = (all_ticks.symbol == 'GCJ7')
     mgc1_symbol = (all_ticks.symbol == 'MGCG7')
-    mgc2_symbol = (all_ticks.symbol == 'MGCH7')
+    mgc2_symbol = (all_ticks.symbol == 'MGCJ7')
 
-    xauusd_bid = pandas.Series(all_ticks[all_ticks_bid][xauusd_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
-    xauusd_ask = pandas.Series(all_ticks[all_ticks_ask][xauusd_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
+    xauusd_bid = pandas.Series(all_ticks[all_ticks_bid][xauusd_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
+    xauusd_ask = pandas.Series(all_ticks[all_ticks_ask][xauusd_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
 
-    gc1_bid = pandas.Series(all_ticks[all_ticks_bid][gc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
-    gc1_ask = pandas.Series(all_ticks[all_ticks_ask][gc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
+    gc1_bid = pandas.Series(all_ticks[all_ticks_bid][gc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
+    gc1_ask = pandas.Series(all_ticks[all_ticks_ask][gc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
     
-    gc2_bid = pandas.Series(all_ticks[all_ticks_bid][gc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
-    gc2_ask = pandas.Series(all_ticks[all_ticks_ask][gc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
+    gc2_bid = pandas.Series(all_ticks[all_ticks_bid][gc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
+    gc2_ask = pandas.Series(all_ticks[all_ticks_ask][gc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
     
-    mgc1_bid = pandas.Series(all_ticks[all_ticks_bid][mgc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
-    mgc1_ask = pandas.Series(all_ticks[all_ticks_ask][mgc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
+    mgc1_bid = pandas.Series(all_ticks[all_ticks_bid][mgc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
+    mgc1_ask = pandas.Series(all_ticks[all_ticks_ask][mgc1_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
     
-    mgc2_bid = pandas.Series(all_ticks[all_ticks_bid][mgc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
-    mgc2_ask = pandas.Series(all_ticks[all_ticks_ask][mgc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price)
+    mgc2_bid = pandas.Series(all_ticks[all_ticks_bid][mgc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
+    mgc2_ask = pandas.Series(all_ticks[all_ticks_ask][mgc2_symbol][no_empty_price].reset_index().drop_duplicates(subset='ts', keep='last').set_index('ts').price).resample('L').pad()
 
     print xauusd_bid
-    xauusd_bid.plot()
-    xauusd_ask.plot()
+    xauusd_bid.resample("S").plot()
+    gc2_bid.resample("S").plot()
+    plt.show()
+
+    (gc2_bid-xauusd_bid).resample("S").plot()
     plt.show()
